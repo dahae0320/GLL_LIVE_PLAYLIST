6985887f50cef16fc1b62d72a6ab435398428244
dahae0320 <ss7ssy@naver.com>
2020-08-27 20:09:38 +0900

    리스트 추가

--- LivePlaylist/urls.py
+++ LivePlaylist/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('playlist/', views.playlist, name='playlist'),
     path('search_result/', views.search_result, name='search_result'),
+    path('road_playlist/', views.road_playlist, name='road_playlist'),
 ]
