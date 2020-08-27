e9efb6e1c30373e7dc4da5682f8eff1d8a645c99
kimseryeong <seryeong7@likelion.org>
2020-08-27 12:02:44 +0900

    ㅇㅏㄴ도

--- LivePlaylist/urls.py
+++ LivePlaylist/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('playlist/', views.playlist, name='playlist'),
+    path('playlist/<int:blog_id>', views.playlist, name='playlist'),
     path('search_result/', views.search_result, name='search_result'),
 ]
