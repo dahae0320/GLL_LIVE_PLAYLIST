c00a0dde579a1067a80cfb1512056afea194dde2
승한 <joe217@likelion.org>
2020-08-28 17:43:26 +0900

    해냈따 내가 해냈어

--- LivePlaylist/static/LivePlaylist/moblie_LivePlaylist.js
+++ LivePlaylist/static/LivePlaylist/moblie_LivePlaylist.js
@@ -1,7 +1,8 @@
 var mql = window.matchMedia("screen and (max-width: 480px)");
 
-mql.addListener(function(e) {
-    if(e.matches) {
+
+// mql.addListener(function (e) {
+    if (mql.matches) {
         console.log('모바일 화면 입니다.');
         // var mySwiper = new Swiper('.swiper-container', {
         //     // Optional parameters
@@ -12,21 +13,21 @@ mql.addListener(function(e) {
         //         el: '.swiper-pagination'
         //     }
         // });
-        $(document).ready(function() {
+        $(document).ready(function () {
             var mySwiper = new Swiper('.swiper-container', {
                 // Optional parameters
                 loop: true,
-        
+
                 // If we need pagination
                 pagination: {
                     el: '.swiper-pagination'
                 }
-        
+
             });
         });
     } else {
         console.log('데스크탑 화면 입니다.');
     }
-});
+// });
 
 
